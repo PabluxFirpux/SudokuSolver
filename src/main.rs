133cb27f8ed2ee8852b22e68d4b982331cfe9b1e
pabluxfirpux <pfirpomolina@gmail.com>
2024-03-26 04:50:21 +0100

    Implemented sudoku logic

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ mod sudoku;
 
 fn main() {
     let mut sudoku = sudoku::make_sudoku(9);
+    sudoku.set_cell(1,1,5);
+    sudoku.set_cell(1,3,7);
     sudoku.print_board();
+    println!("{:?}",sudoku.get_square(4));
 }
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -7,7 +7,9 @@ pub struct Sudoku {
 }
 
 impl Sudoku {
-    pub fn set_cell(&mut self, x: u32, y: u32, value: u32) {
+    pub fn set_cell(&mut self, mut x: u32, mut y: u32, value: u32) {
+        x = x - 1;
+        y = y - 1;
         let mut cell = &mut self.board[x as usize][y as usize];
         cell.set_value(value);
     }
@@ -17,8 +19,38 @@ impl Sudoku {
             for j in 0..self.size {
                 print!("{}", self.board[i as usize][j as usize].value)
             }
-            println!("")
+            println!();
         }
+    }
+
+    pub fn get_row(&mut self, mut n: u32) -> &Vec<Cell> {
+        n = n - 1;
+        &self.board[n as usize]
+    }
+
+    //Maybe problems later
+    pub fn get_column(&mut self, mut n: u32) -> Vec<&Cell> {
+        n = n - 1;
+        let mut vec = Vec::new();
+        for i in 0..self.size {
+            vec.push(&self.board[i as usize][n as usize])
+        }
+        vec
+    }
+
+    pub fn get_square(&mut self, mut n: u32) -> Vec<Vec<&Cell>> {
+        let mut square = Vec::with_capacity(3);
+        n = n-1;
+        let mut x = (n/3)*3;
+        let mut y = (n % 3)*3;
+        for i in x..x+3 {
+            let mut line = Vec::with_capacity(3);
+            for j in y..y+3 {
+                line.push(&self.board[i as usize][j as usize])
+            }
+            square.push(line);
+        }
+        square
     }
 }
 
