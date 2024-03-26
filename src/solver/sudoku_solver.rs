e9623ef7ea79e5bf982b374f0b222f656c20748a
pabluxfirpux <pfirpomolina@gmail.com>
2024-03-27 00:13:18 +0100

    MVP

--- src/solver/sudoku_solver.rs
+++ src/solver/sudoku_solver.rs
@@ -29,10 +29,97 @@ impl Shadow {
 }
 
 pub fn solve(mut sudoku: Sudoku) {
+    println!("Unsolved sudoku:");
     sudoku.print_board();
     println!("===========================");
-    let mut shadow = get_shadow(9, &mut sudoku);
-    shadow.print_board();
+    println!("Solved sudoku:");
+    let mut changes = 0;
+    let mut last_changes = 0;
+    let mut shadows = Vec::new();
+    for i in 1..=9{
+        shadows.push(get_shadow(i, &mut sudoku));
+    }
+    changes = apply_shadows(&mut sudoku, shadows);
+    while (changes > last_changes) {
+        let mut shadows = Vec::new();
+        for i in 1..=9{
+            shadows.push(get_shadow(i, &mut sudoku));
+        }
+        last_changes = changes;
+        changes = apply_shadows(&mut sudoku, shadows)+last_changes;
+
+    }
+    sudoku.print_board();
+}
+
+fn apply_shadows(sudoku: &mut Sudoku, shadows: Vec<Shadow>) -> u32 {
+    let mut x = 0;
+    for shadow in 0..shadows.len(){
+        x+=apply_shadow(sudoku, &shadows[shadow]);
+    }
+    x
+}
+
+fn apply_shadow(sudoku: &mut Sudoku, shadow: &Shadow) -> u32 {
+    let mut x = 0;
+    for i in 1..=9 {
+        let mut to_change = Vec::new();
+        if is_only(&shadow.board[i-1]){
+            x+=1;
+            for j in 0..9{
+                to_change.push((i-1,j))
+            }
+        }
+
+
+        let mut vec = Vec::new();
+        for j in 0..9 {
+            vec.push(shadow.board[j][i-1])
+        }
+        if is_only(&vec){
+            x+=1;
+            for j in 0..9 {
+                to_change.push((j,i-1))
+            }
+        }
+
+
+        let square = sudoku.get_square(i as u32);
+        let mut vec2 = Vec::new();
+        for row in &square{
+            for cell in row{
+                vec2.push(shadow.board[(cell.x_pos-1) as usize][(cell.y_pos-1) as usize]);
+            }
+        }
+        if is_only(&vec2){
+            x+=1;
+            for row in square{
+                for cell in row{
+                    to_change.push(((cell.x_pos - 1) as usize, (cell.y_pos - 1) as usize));
+                }
+            }
+        }
+        apply_changes(sudoku, shadow, to_change);
+    }
+    x
+}
+
+fn apply_changes(sudoku: &mut Sudoku, shadow: &Shadow, coords: Vec<(usize,usize)>) {
+    for (x,y) in coords {
+        if shadow.board[x][y] {
+            sudoku.set_cell((x + 1) as u32, (y + 1) as u32, shadow.num);
+        }
+    }
+}
+
+fn is_only(bools: &Vec<bool>) -> bool {
+    let mut x = 0;
+    for b in bools {
+        if *b {
+            x+=1;
+        }
+    }
+    x == 1
 }
 
 fn get_shadow(num: u32, sudoku: &mut Sudoku) -> Shadow {
